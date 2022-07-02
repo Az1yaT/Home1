174b6e82dd81128b0a61e7af8bd25dd6a291bd28
Az1yaT <jeenbek0vv.06@gmail.com>
2022-07-02 22:55:50 +0600

    Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n > 100)
+{
+    Console.WriteLine((n / 1) % 10);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
