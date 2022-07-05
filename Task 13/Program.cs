08fcfbc896d98fc7311c0c036a9200cd0efb604a
Az1yaT <jeenbek0vv.06@gmail.com>
2022-07-05 21:10:46 +0600

    HomeWork

---
+++ Task 13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int newNum = 0;
+
+if(n < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+while(n > 99)
+{
+    newNum = n % 10;
+    n = n / 10;
+    Console.WriteLine($"Третья цифра числа = {newNum}");
+    return;
+}
---
+++ Task 15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число дня недели:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1:case 2:case 3:case 4:case 5: Console.WriteLine("Нет");
+    break;
+    case 6:case 7: Console.WriteLine("Да");
+    break;
+    default: Console.WriteLine("Нет такого дня недели");
+    break;
+}
+
